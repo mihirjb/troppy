c441275e42ce3a886aa11e67488070d0d2792029
Mihir <mihirbuch@Mihirs-MacBook-Air.local>
2013-05-12 01:02:59 +0530

    Postgres changes 17

--- app/controllers/lrs_controller.rb
+++ app/controllers/lrs_controller.rb
@@ -8,7 +8,7 @@ class LrsController < ApplicationController
      @payvoucher = Payvoucher.all
      if params[:lrno]
          lrs=Lr.arel_table
-@lr = Lr.find(:all, :conditions => ['lrno LIKE ? AND lower(party) LIKE lower(?)', "%#{params[:lrno].to_i}%", "%#{params[:company].downcase}%"])
+@lr = Lr.find(:all, :conditions => ['lrno == ? ', "#{params[:lrno]}"])
    
     #    @lr = Lr.where(lrs[:lrno].matches("%#{params[:lrno]}%").and(lrs[:party].matches("%#{params[:company]}%")))
    else
